5fbfa4953429d7c597e01b63da4e92f31eaddcf8
mobis <mobiskif@hotmail.com>
2020-04-09 07:05:33 +0300

    8

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 import javax.swing.*;
-import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.applet.Applet;
+import java.awt.*;
 
-public class Main {
+public class Main extends Applet {
+    public Main() throws HeadlessException {
+        super();
+        setLayout( new BorderLayout());
+        add(new MainForm().$$$getRootComponent$$$());
+        setVisible(true);
+    }
 
     public static void main(String[] args) {
         JFrame f = new JFrame("Lennard-Jones");
@@ -11,16 +16,5 @@ public class Main {
         f.add(new MainForm().$$$getRootComponent$$$());
         f.pack();
         f.setVisible(true);
-
-        //System.out.println("Working Directory is: " + System.getProperty("user.dir"));
-
-        Path currentRelativePath = Paths.get("");
-        String s = currentRelativePath.toAbsolutePath().toString();
-        //System.out.println("Current relative path is: " + s);
-
-        //System.out.println(Paths.get(".").toAbsolutePath().normalize().toString());
-
-        URL location = Main.class.getProtectionDomain().getCodeSource().getLocation();
-        //System.out.println(location.getFile());
     }
 }
